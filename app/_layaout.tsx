1e7d33df602c6b95eb58daa3ceab1f2304d9b0d4
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-24 09:28:00 -0400

    firts commit

---
+++ app/_layaout.tsx
@@ -0,0 +1,16 @@
+import {View, Text} from "react-native";
+import React from "react";
+import "../global.css";
+
+
+const _layaout = () => {
+    return(
+        <View>
+            <Text>
+            _layaout
+            </Text>
+        </View>
+    )
+}
+
+export default _layaout;
